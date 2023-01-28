27fb180ac406b2f1e029396573e390e5a79fd6ce
Mark Lin <mark.lin2234@gmail.com>
2023-01-28 18:54:51 -0500

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,51 +1,62 @@
 import React, { Component } from 'react';
 
 class About extends Component {
-  render() {
+	render() {
+		if (this.props.data) {
+			var name = this.props.data.name;
+			var profilepic = 'images/' + this.props.data.image;
+			// var bio = this.props.data.bio;
+			var city = this.props.data.address.city;
+			var state = this.props.data.address.state;
+			var phone = this.props.data.phone;
+			var email = this.props.data.email;
+			var resumeDownload = this.props.data.resumedownload;
+		}
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
-      // var bio = this.props.data.bio;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
-    }
+		return (
+			<section id="about">
+				<div className="row">
+					<div className="three columns">
+						<img className="profile-pic" src={profilepic} alt="Profile Pic" />
+					</div>
+					<div className="nine columns main-col">
+						<h2>About Me</h2>
 
-    return (
-      <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
-
-            <p>I love music, reading and (obviously) coding. I excel at front-end frameworks like <span style={{color: 'white'}}>React.js and Angular.js</span> and have back-end experience with <span style={{color: 'white'}}>SQL, GraphQL, Firebase and Java/Scala</span>. I am currently looking for an internship for Summer 2023.</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						         {city}, {state}<br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button" target="_blank" rel="noreferrer"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
-            </div>
-         </div>
-      </div>
-
-   </section>
-    );
-  }
+						<p>
+							I love music, reading and (obviously) coding. I excel at front-end frameworks like{' '}
+							<span style={{ color: 'white' }}>React.js and Angular.js</span> and have back-end
+							experience with{' '}
+							<span style={{ color: 'white' }}>
+								Ruby on Rails, AWS, GraphQL, Firebase and Java/Scala
+							</span>
+							. I am currently looking for an internship for Summer 2023.
+						</p>
+						<div className="row">
+							<div className="columns contact-details">
+								<h2>Contact Details</h2>
+								<p className="address">
+									<span>{name}</span>
+									<br />
+									{city}, {state}
+									<br />
+									<span>{phone}</span>
+									<br />
+									<span>{email}</span>
+								</p>
+							</div>
+							<div className="columns download">
+								<p>
+									<a href={resumeDownload} className="button" target="_blank" rel="noreferrer">
+										<i className="fa fa-download"></i>Download Resume
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default About;
